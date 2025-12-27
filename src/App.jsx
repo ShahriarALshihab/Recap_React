3ffba839de09cf7721e20100b139c50650ce56f5
ShahriarAlShihab <shahriarshihab96@gmail.com>
2025-12-27 19:48:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Section1 from './Components/Section1/Section1'
+import Section2 from './Components/Section2/Section2'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <Section1></Section1>
+      <Section2></Section2>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h4>Target Audience</h4>
+        <button></button>
+    </div>
+  )
+}
+
+export default Navbar
